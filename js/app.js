12eebc8259da8f2cd4bdc9b306a70d798a8116ec
arifvtti <arif.vtti@gmail.com>
2022-03-15 08:59:49 +0600

    data view in ui

--- js/app.js
+++ js/app.js
@@ -7,8 +7,10 @@ const loadCountries = () => {
 }
 
 const showCountries = countries => {
-    console.log(countries);
+    console.log(countries[0]);
     const countriesHTML = countries.map(country => getHTML(country));
+    const container = document.getElementById("container");
+    container.innerHTML = countriesHTML.join(" ");
     console.log(countriesHTML);
 }
 
@@ -16,6 +18,8 @@ const getHTML = country => {
     return `
         <div>
             <h3>Country name : ${country.name.common}</h3>
+            <h4>Capital name : ${country.capital}</h4>
+            <img src="${country.flags.png}"
         </div>
     
     `
